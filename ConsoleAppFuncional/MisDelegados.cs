669d62f2888bfd061a60ad0b4ac5307a39ce184e
david0896 <davidvasquez0896@gmail.com>
2022-02-24 16:15:37 -0400

    Delegados

---
+++ ConsoleAppFuncional/MisDelegados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFuncional
+{
+    class MisDelegados
+    {
+        //delegado 
+        public delegate float DelegadoOperacion(float valor1, float valor2);
+
+        //ejercicio cajero
+        //deposito
+        public float deposito(float cantidad, float monto) {
+            Console.WriteLine("Deposito realizado");
+            return cantidad + monto;
+        }
+
+        public float retiro(float cantidad, float monto) {
+            if (cantidad > monto) {
+                Console.WriteLine("No es posible hacer el retiro");
+                return 0.0f;
+            }
+
+            Console.WriteLine("Retiro realizado");
+            return monto - cantidad;
+        }
+
+        public float RealizaOperacion(DelegadoOperacion dOperacion, float cantidad, float monto) {
+            Console.WriteLine("Operacion iniciada");
+            return dOperacion(cantidad, monto);
+        }
+
+    }
+}
--- ConsoleAppFuncional/Program.cs
+++ ConsoleAppFuncional/Program.cs
@@ -6,14 +6,24 @@ namespace ConsoleAppFuncional
     {
         static void Main(string[] args)
         {
+            //lambdasExpression();
+            MisDelegados myDelegates = new MisDelegados();
+            MisDelegados.DelegadoOperacion retiro = myDelegates.retiro;
+            MisDelegados.DelegadoOperacion deposito = myDelegates.deposito;
+            Console.WriteLine("Saldo restante: " + myDelegates.RealizaOperacion(deposito, 1001, 1000));
+            Console.WriteLine("Saldo restante: " + myDelegates.RealizaOperacion(retiro, 1001, 1000));
+
+        }
+
+        public static void lambdasExpression() {
             //lambda = funcion anonima 
             //(parametros) => el cuerpo de la funcion 
             int inValor = 0;
             int valorUno = 0;
             int valorDos = 0;
             string resultado = "";
-            
-            Func<int, bool> evaluaPar =  numero => numero % 2 == 0;
+
+            Func<int, bool> evaluaPar = numero => numero % 2 == 0;
 
             Console.WriteLine("Ingrese un valor para evaluar si es par o no");
 
@@ -21,7 +31,7 @@ namespace ConsoleAppFuncional
 
             resultado = evaluaPar(inValor) == true ? "es un numero par" : "no es un numero par";
 
-            Console.WriteLine("el numero inresado " + inValor + ": " + resultado );
+            Console.WriteLine("el numero inresado " + inValor + ": " + resultado);
 
             Console.ReadLine();
 
